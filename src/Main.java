e2478825d702ae4905b078439a4161f5757bbbba
ThiagoPerdigao <thiagovitor299@gmail.com>
2023-08-30 17:10:29 -0300

    classe relogio

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,64 @@
 package src;
-
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Relogio relogio = new Relogio(0, 0, 0); // Inicializa com meia-noite
+
+        Scanner scanner = new Scanner(System.in);
+
+        int opcao;
+        do {
+            System.out.println("Escolha uma opção:");
+            System.out.println("1 - Ajustar Hora");
+            System.out.println("2 - Reiniciar para Meia-Noite");
+            System.out.println("3 - Marcar Intervalo");
+            System.out.println("4 - Imprimir Hora (Formato 24h)");
+            System.out.println("5 - Imprimir Hora (Formato AM/PM)");
+            System.out.println("0 - Sair");
+            opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("Informe a nova hora: ");
+                    int novaHora = scanner.nextInt();
+                    System.out.println("Informe o novo minuto: ");
+                    int novoMinuto = scanner.nextInt();
+                    System.out.println("Informe o novo segundo: ");
+                    int novoSegundo = scanner.nextInt();
+                    relogio.acertaHora(novaHora, novoMinuto, novoSegundo);
+                    System.out.println("Hora ajustada com sucesso!");
+                    break;
+                case 2:
+                    relogio.reiniciarParaMeiaNoite();
+                    System.out.println("Relógio reiniciado para meia-noite!");
+                    break;
+                case 3:
+                    System.out.println("Informe a hora, minuto e segundo iniciais: ");
+                    int horaInicial = scanner.nextInt();
+                    int minutoInicial = scanner.nextInt();
+                    int segundoInicial = scanner.nextInt();
+                    System.out.println("Informe a hora, minuto e segundo finais: ");
+                    int horaFinal = scanner.nextInt();
+                    int minutoFinal = scanner.nextInt();
+                    int segundoFinal = scanner.nextInt();
+                    relogio.marcarIntervalo(horaInicial, minutoInicial, segundoInicial, horaFinal, minutoFinal, segundoFinal);
+                    System.out.println("Intervalo marcado com sucesso!");
+                    break;
+                case 4:
+                    System.out.println("Hora Formatada (24h): " + relogio.imprimirHora(true));
+                    break;
+                case 5:
+                    System.out.println("Hora Formatada (AM/PM): " + relogio.imprimirHora(false));
+                    break;
+                case 0:
+                    System.out.println("Saindo do programa.");
+                    break;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
+        } while (opcao != 0);
+
+        scanner.close();
+    }
 }
--- src/Relogio.java
+++ src/Relogio.java
@@ -1,6 +1,61 @@
 package src;
 
 public class Relogio {
-   
+    private int hora;
+    private int minuto;
+    private int segundo;
+
+    public Relogio(int hora, int minuto, int segundo) {
+        this.hora = hora;
+        this.minuto = minuto;
+        this.segundo = segundo;
+    }
+
+    public void acertaHora(int novaHora, int novoMinuto, int novoSegundo) {
+        this.hora = novaHora;
+        this.minuto = novoMinuto;
+        this.segundo = novoSegundo;
+    }
+
+    public void reiniciarParaMeiaNoite() {
+        this.hora = 0;
+        this.minuto = 0;
+        this.segundo = 0;
+    }
+
+    public void marcarIntervalo(int horaInicial, int minutoInicial, int segundoInicial,
+                            int horaFinal, int minutoFinal, int segundoFinal) {
+    int segundosInicio = horaInicial * 3600 + minutoInicial * 60 + segundoInicial;
+    int segundosFim = horaFinal * 3600 + minutoFinal * 60 + segundoFinal;
+
+    int diferencaSegundos = segundosFim - segundosInicio;
+
+    this.hora = diferencaSegundos / 3600;
+    this.minuto = (diferencaSegundos % 3600) / 60;
+    this.segundo = diferencaSegundos % 60;
+}
+
+    public String imprimirHora(boolean formato24h) {
+        if (formato24h) {
+            return String.format("%02d:%02d:%02d", hora, minuto, segundo);
+        } else {
+            String ampm = hora < 12 ? "AM" : "PM";
+            int hora12h = hora > 12 ? hora - 12 : hora;
+            return String.format("%02d:%02d:%02d %s", hora12h, minuto, segundo, ampm);
+        }
+    }
+
+    public int getHora() {
+        return hora;
+    }
+
+    public int getMinuto() {
+        return minuto;
+    }
+
+    public int getSegundo() {
+        return segundo;
+    }
+
 }
 
